934f0a82fb3bfea34b96369d0c21e02f720535dd
Thiago Nogueira <thiagotn@gmail.com>
2013-11-04 15:05:54 -0200

    Fix name of method

--- src/main/java/poc/beanshell/SampleTask.java
+++ src/main/java/poc/beanshell/SampleTask.java
@@ -1,6 +1,8 @@
 package poc.beanshell;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class SampleTask implements Task {
 
@@ -32,5 +34,14 @@ public class SampleTask implements Task {
 
     public Date getProcessedTime() {
         return updated;
+    }
+
+    public boolean hasMoreThan3Months() {
+        GregorianCalendar moreThan3Months = new GregorianCalendar();
+        moreThan3Months.setTime(this.created);
+        moreThan3Months.add(Calendar.MONTH, 3);
+
+        GregorianCalendar now = new GregorianCalendar();
+        return moreThan3Months.after(now);
     }
 }
